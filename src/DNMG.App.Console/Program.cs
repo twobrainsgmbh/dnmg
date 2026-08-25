a5224413dcfb6e47ee8353146c07c266bc12213c
Stefan Fiedler <stefan@twobrains.com>
2026-08-25 20:44:30 +0200

    fix(input): handle special case 0x00

--- src/DNMG.App.Console/Program.cs
+++ src/DNMG.App.Console/Program.cs
@@ -18,7 +18,7 @@ static internal class Program
 		var totalCycles = 0;
 
 		// Set default Joypad state to "No buttons pressed"
-		input.PressedKeys = 0;
+		input.PressedButtons = 0;
 
 #if WINDOWS
 		// calling XInputGetState comes with a cost we only want to pay if a controller is actually attached.
@@ -41,22 +41,22 @@ static internal class Program
 				if (useController)
 				{
 					// experimental XBOX controller support
-					var keys = default(Input.JoypadKeys);
+					var keys = default(Input.JoypadButtons);
 					Windows.Win32.PInvoke.XInputGetState(0, out var state);
-					foreach (var vy in new ReadOnlySpan<ValueTuple<XINPUT_GAMEPAD_BUTTON_FLAGS, Input.JoypadKeys>>([
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_LEFT, Input.JoypadKeys.LeftArrow),
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_RIGHT, Input.JoypadKeys.RightArrow),
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_UP, Input.JoypadKeys.UpArrow),
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_DOWN, Input.JoypadKeys.DownArrow),
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_A, Input.JoypadKeys.A),
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_B, Input.JoypadKeys.B),
-						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_START, Input.JoypadKeys.Start)
+					foreach (var vy in new ReadOnlySpan<ValueTuple<XINPUT_GAMEPAD_BUTTON_FLAGS, Input.JoypadButtons>>([
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_LEFT, Input.JoypadButtons.LeftArrow),
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_RIGHT, Input.JoypadButtons.RightArrow),
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_UP, Input.JoypadButtons.UpArrow),
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_DPAD_DOWN, Input.JoypadButtons.DownArrow),
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_A, Input.JoypadButtons.A),
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_B, Input.JoypadButtons.B),
+						(XINPUT_GAMEPAD_BUTTON_FLAGS.XINPUT_GAMEPAD_START, Input.JoypadButtons.Start)
 					]))
 					{
 						if (state.Gamepad.wButtons.HasFlag(vy.Item1))
 							keys |= vy.Item2;
 					}
-					input.PressedKeys = keys;
+					input.PressedButtons = keys;
 				}
 #endif
 			}
--- src/DNMG/Input.cs
+++ src/DNMG/Input.cs
@@ -1,9 +1,10 @@
 namespace DNMG;
 
+// SEE: https://gbdev.io/pandocs/Joypad_Input.html
 public sealed class Input
 {
 	[Flags]
-	public enum JoypadKeys // lower nibble is dpad, upper nibble is action buttons
+	public enum JoypadButtons // lower nibble is dpad, upper nibble is action buttons
 	{
 		None,
 		RightArrow = 1 << 0,
@@ -20,24 +21,29 @@ public sealed class Input
 
 	private byte JoyPadRegister { get => _cpu.Memory[0xFF00]; set => _cpu.Memory[0xFF00] = value; }
 
-	public JoypadKeys PressedKeys { get; set { field = value; UpdateJoyPadRegister(JoyPadRegister); } }
+	public JoypadButtons PressedButtons { get; set { field = value; UpdateJoyPadRegister(JoyPadRegister); } }
 
 	public Input(Cpu cpu)
 	{
 		_cpu = cpu;
 		cpu.OnMemoryWrite[0x00] = UpdateJoyPadRegister;
+		UpdateJoyPadRegister(0);
 	}
 
 	private void UpdateJoyPadRegister(byte value)
 	{
-		// only bits 4 and 5 (button group select) are writable by the program
-		value &= 0x30;
-		// upper 2 bits are always 1, next 2 bits select button group, lower 4 bits are button states (low active)
-		JoyPadRegister = (byte)(0xC0 | value | value switch
-		{
-			0x10 => (~(int)PressedKeys >> 4) & 0xF, // action buttons
-			0x20 => (~(int)PressedKeys) & 0xF, // dpad buttons
-			_ => 0xF // neither selected
-		});
+		var pressedButtons = (int)PressedButtons;
+		var buttonMask = 0; // default for no buttons pressed
+		if ((value & (1 << 4)) == 0) // dpad buttons selected?
+			buttonMask = pressedButtons;
+		if ((value & (1 << 5)) == 0) // action buttons selected?
+			buttonMask |= pressedButtons >> 4;
+		// buttons are low active!
+		buttonMask = ~buttonMask & 0xF;
+		// request joypad interrupt if no buttons were pressed before and now at least one is
+		if ((JoyPadRegister & 0xF) == 0xF && buttonMask != 0xF)
+			_cpu.RequestInterrupt(Cpu.IFBits.Joypad);
+		// upper 2 bits are always 1, next 2 bits select button group (writable by the program), lower 4 bits are button states
+		JoyPadRegister = (byte)(0b1100_0000 | (value & 0x30) | buttonMask);
 	}
 }
