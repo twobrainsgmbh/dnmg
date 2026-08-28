7eb402d2c3494bbaf792163783fde99d39341ff8
Stefan Fiedler <stefan@twobrains.com>
2026-08-28 12:35:25 +0200

    perf: highly optimized sprite renderer (~3.25x speedup)

--- src/DNMG.App.Console/Program.cs
+++ src/DNMG.App.Console/Program.cs
@@ -101,7 +101,7 @@ internal static class Program
 					var c = (char)0;
 					for (int subY = 0; subY < 6; subY++)
 					{
-						var pixelColor = gpu.FrameBuffer[x / scale, (y + subY) / scale];
+						var pixelColor = gpu.FrameBuffer[(y + subY) / scale, x / scale];
 						if (pixelColor == color)
 							c |= (char)(1 << subY);
 					}
